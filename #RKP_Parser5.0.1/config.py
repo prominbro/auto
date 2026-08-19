bcab4d7f86fce47fbf1761d918bcfec50a03d858
prominbro <kberonek@gmail.com>
2026-08-20 01:38:22 +0500

    fix: only result files

--- #RKP_Parser5.0.1/config.py
+++
@@ -1,7 +0,0 @@
-API_ID = 12345678                                        # API ID, получите свой на my.telegram.org
-API_HASH = 'a1b2c3d4a1b2c3d4a1b2c3d4a1b2c3d4'            # API hash, получите свой на my.telegram.org
-SESSION_NAME = 'myparser'                                # Имя сессии
-# Прокси для работы TG (в условиях блокировок), прокси должен работать от tg ws proxy
-TG_MTProto_SERVER = "127.0.0.1"                          # IP прокси
-TG_MTProto_PORT = 1080                                   # Порт прокси
-TG_MTProto_SECRET = "81499d5d5b1df380917b0579ea4d3788"   # Секрет прокси
